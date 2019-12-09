9c22ebb947c37b31b28f646e152d8a4c1ffb7cef
DavideCristoni <davide@cristoni.it>
2019-12-09 17:06:23 +0100

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <conio.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	char in;
+	in = _getch();
+	while (in != 'q') {
+		cout << "hai digitato " << in << endl;
+		in = _getch();
+	}
+}
