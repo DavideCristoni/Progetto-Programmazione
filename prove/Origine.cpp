e991a5e44e8f4fb93827d4ada7bc744b313e04e0
DavideCristoni <davide@cristoni.it>
2020-01-10 18:29:29 +0100

    Update Origine.cpp

--- prove/Origine.cpp
+++ prove/Origine.cpp
@@ -1,14 +1,41 @@
-#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS_
 
 #include <stdio.h>
 #include <iostream>
 #include <conio.h>
 #include <stdlib.h>
 
-int main() {
-	FILE* f;
-	f=fopen("ciao.txt", "a"); //prevede che il file esista
-	fprintf(f, "%d", 10);
-	fclose(f);
+using namespace std;
 
+struct mappa {
+	char map[15];
+	int pos = 7;
+};
+
+int main() {   //movimento con a e d
+	mappa m;
+	for (int i = 0; i < 5; i++)
+		m.map[i] = ' ';
+	m.map[m.pos] = 'A';
+	char in;
+	in = _getch();
+	while (in != 'q') {
+		if (in == 'a') {
+			m.map[m.pos] = ' ';
+			if (m.pos != 0)
+				m.pos--;
+			m.map[m.pos] = 'A';
+		}
+		else if (in == 'd') {
+			m.map[m.pos] = ' ';
+			if (m.pos != 14)
+				m.pos++;
+			m.map[m.pos] = 'A';
+		}
+		cout << m.map << endl;
+		cout << in << endl;
+		in = _getch();
+		system("clear");
+		
+	}
 }
