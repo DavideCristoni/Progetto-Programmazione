71f01cbc6d95d0aabdd30c29d704d309644ace0c
DavideCristoni <davide@cristoni.it>
2020-01-18 15:56:45 +0100

    agginta la classe oggetto in formato importabile
    la classe in se non è finale ma i meccanismi principali sono pressocchè quelli.

---
+++ finale/oggetto.cpp
@@ -0,0 +1,57 @@
+#include "oggetto.h"
+#include <iostream>
+
+using namespace std;
+
+	oggetto::oggetto(int type, int x, int y, int offx, int offy, int score, const int battery, const char txtr[10][10], bool disappear)
+	{
+		this->type = type;
+		posx = x;
+		globaly = y;
+		posy = 0;
+		offsetX = offx;
+		offsetY = offy;
+		this->score = score;
+		this->battery = battery;
+		enable = true;
+		disappearOnContact = disappear;
+		for (int i = 0; i < 10; i++)
+			strcpy(texture[i], txtr[i]);
+	}
+	void oggetto::move()
+	{
+		posy++;
+	}
+	void oggetto::stampa()
+	{
+		cout << "posx: " << posx << ", globaly: " << globaly << ", score: " << score << ", battery: " << battery << " texture:" << endl;
+		for (int i = 0; i < offsetY; i++) {
+			for (int j = 0; j < offsetX; j++)
+				cout << texture[i][j];
+			cout << endl;
+		}
+		/*
+		int i;
+		int j = 0;
+			if(!disappearOnContact || enable){
+			attron(COLOR_PAIR(type));
+				for(i = offsetY - 1; i >= 0; i --)
+				{
+					while(j < 10  && texture[i][j] != ' ')
+					{
+						mvprintw(posy - i, posx + j, "%c", texture[i][j]);
+						j++;
+					}
+					j = 0;
+				}
+		attroff(COLOR_PAIR(type));
+		}*/
+	}
+	void oggetto::effect(int& score, int& battery)
+	{
+		if (enable) {
+			score = score - this->score;
+			battery = battery + this->battery;
+			enable = false;
+		}
+	}
---
+++ finale/oggetto.h
@@ -0,0 +1,39 @@
+#ifndef OGGETTO_H
+
+#define OGGETTO_H 
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+const int BATTERY = 15;
+const char BATTERYTXTR[10][10] = { "B        ", "         ", "         ", "         ", "         ", "         ", "         ", "         ", "         ", "         ", };
+const int BUCA = 15;
+const char BUCATXTR[10][10] = { "^^^^     ", "^^^^     ", "^^^^     ", "         ", "         ", "         ", "         ", "         ", "         ", "         " };
+
+class oggetto {
+protected:
+	int posx, posy, globaly;
+	int offsetX, offsetY;
+	int  type, score, battery;
+	bool enable, disappearOnContact;
+	char texture[10][10];
+
+public:
+
+	oggetto(int type, int x, int y, int offx, int offy, int score, const int battery, const char txtr[10][10], bool disappear);
+
+	void move();
+
+	void stampa();
+
+	void effect(int& score, int& battery);
+
+};
+
+struct lista {
+	oggetto* val;
+	lista* next;
+};
+
+typedef lista* listpntr;
+
+#endif
