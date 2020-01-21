acf007d7c5982ab795549edc9686dd827232c37e
elements72 <50247853+elements72@users.noreply.github.com>
2020-01-21 11:42:02 +0100

    Aggiunto il Makefile e spacchetta la classe player

---
+++ prove/Versione1/player.h
@@ -0,0 +1,18 @@
+#include "header.h"
+#define OFFSET_MENU 30
+
+
+class player
+{
+    protected:
+        int posx, posy;
+        int offsetX, offsetY;
+    public:
+        player(int x, int y);
+        void move(char direzione, int bordoX, int bordoY);
+        void stampa();
+        int getPosx();
+        int getPosy();
+        int getOffsetx();
+        int getOffsety();
+};
